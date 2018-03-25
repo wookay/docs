d294112c3beb3c7fd6b366c0bf330152ff3d168b
WooKyoung Noh <wookay.noh@gmail.com>
2018-03-26 02:57:24 +0900

    Bukdu.HTML5.Form

--- Bukdu.jl/search_index.js
+++ Bukdu.jl/search_index.js
@@ -13,7 +13,55 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Bukdu.jl",
     "category": "section",
-    "text": "Bukdu 🌌  is a web development framework for Julia.It's influenced by Phoenix framework.importall Bukdu\n\ntype WelcomeController <: ApplicationController\nend\n\nindex(::WelcomeController) = \"hello world\"\n\nRouter() do\n    get(\"/\", WelcomeController, index)\nend\n\nBukdu.start(8080)"
+    "text": "Bukdu 🌌  is a web development framework for Julia.It\'s influenced by Phoenix framework.using Bukdu\n\nstruct WelcomeController <: ApplicationController\n    conn::Conn\nend\n\nindex(::WelcomeController) = \"hello world\"\n\nRouter() do\n    get(\"/\", WelcomeController, index)\nend\n\nBukdu.start(8080)"
+},
+
+{
+    "location": "render/#",
+    "page": "Render",
+    "title": "Render",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "render/#Bukdu.render",
+    "page": "Render",
+    "title": "Bukdu.render",
+    "category": "function",
+    "text": "render(::Type{Text}, data)::Render\n\n\n\n\n\nrender(::Type{HTML}, data)::Render\n\n\n\n\n\nrender(::Type{JSON}, data)::Render\n\n\n\n\n\nrender(::Type{JavaScript}, data)::Render\n\n\n\n\n\n"
+},
+
+{
+    "location": "render/#Render-1",
+    "page": "Render",
+    "title": "Render",
+    "category": "section",
+    "text": "Bukdu.render"
+},
+
+{
+    "location": "HTML5/Form/#",
+    "page": "HTML5.Form",
+    "title": "HTML5.Form",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "HTML5/Form/#Bukdu.HTML5.Form.change",
+    "page": "HTML5.Form",
+    "title": "Bukdu.HTML5.Form.change",
+    "category": "function",
+    "text": "change\n\n\n\n\n\n"
+},
+
+{
+    "location": "HTML5/Form/#Bukdu.HTML5.Form-1",
+    "page": "HTML5.Form",
+    "title": "Bukdu.HTML5.Form",
+    "category": "section",
+    "text": "Bukdu.HTML5.Form.change"
 },
 
 ]}
