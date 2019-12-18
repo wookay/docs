af8d657fb563d79f6ae09609d35f391545d5c0fd
WooKyoung Noh <wookay.noh@gmail.com>
2019-12-19 03:05:37 +0900

    Calendars

---
+++ Calendars.jl/Times.jl
@@ -0,0 +1,8 @@
+Times
+
+```@docs
+Dates.Day(n::Float64)::Union{Period,CompoundPeriod}
+Dates.Hour(n::Float64)::Union{Period,CompoundPeriod}
+Dates.Minute(n::Float64)::Union{Period,CompoundPeriod}
+Dates.Second(n::Float64)::Union{Period,CompoundPeriod}
+```
