c61213e733cb227ae52c2749b86f1c262468c249
WooKyoung Noh <wookay.noh@gmail.com>
2026-08-03 22:35:18 +0900

    add  EnvironmentVariables.jl Docs

---
+++ EnvironmentVariables.jl/Docs.jl
@@ -0,0 +1,13 @@
+### EnvironmentVariables.Docs
+
+```julia-repl
+julia> using EnvironmentVariables.Docs
+
+julia> JULIA_EDITOR
+  The editor returned by InteractiveUtils.editor() and used in, e.g., InteractiveUtils.edit, referring to the command of the preferred editor, for instance vim. $JULIA_EDITOR takes precedence over
+  $VISUAL, which in turn takes precedence over $EDITOR. If none of these environment variables is set, then the editor is taken to be open on Windows and OS X, or /etc/alternatives/editor if it
+  exists, or emacs otherwise. To use Visual Studio Code on Windows, set $JULIA_EDITOR to code.cmd.
+
+julia> JULIA_DEBUG
+  Enable debug logging for a file or module, see Logging for more information.
+```
